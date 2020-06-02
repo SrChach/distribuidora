635d2ee7cf4418d24e62990550f0205c84483a66
Mario Nataren <mario.nataren@gmail.com>
2020-06-01 23:41:37 -0500

    Trabaje en el componente quienes somos, agregue el texto del cliente e imagenes

--- src/components/QuienesSomos.vue
+++ src/components/QuienesSomos.vue
@@ -1,48 +1,57 @@
 <template>
-    
-    <div class="columns">
-        <div class="column">
-            <span class="title is-6 is-mega-menu-title">Ejemplo de contenido</span>
-            <a class="navbar-item" href="#">
-                <div class="navbar-content">
-                    <p>
-                        <small class="has-text-info">03 Aug 2017</small>
-                    </p>
-                    <p>Característica</p>
-                </div>
-            </a>
-            <a class="navbar-item" href="#">
-                <div class="navbar-content">
-                    <p>
-                        <small class="has-text-info">03 Aug 2017</small>
-                    </p>
-                    <p>Otra característica</p>
-                </div>
-            </a>
-            <a class="navbar-item" href="#">
-                <div class="navbar-content">
-                    <p>
-                        <small class="has-text-info">03 Aug 2017</small>
-                    </p>
-                    <p>Ultima característica</p>
-                </div>
-            </a>
+    <div class="container">
+    <div class="columns is-vcentered">
+        <div class="column is-full-mobile is-one-third-tablet is-one-third-desktop is-one-third-widescreen is-one-third-fullhd">
+                    <br>
+                    <figure class="image is-2by1">
+                        <img src="../assets/carrusel/img_cinco.jpeg" alt="imagen 5">
+                    </figure>
         </div>
-        <div class="column">
+        <div class="column is-full-mobile is-one-third-tablet is-one-third-desktop is-one-third-widescreen is-one-third-fullhd">
             <span class="title is-6 is-mega-menu-title">
-                Las columnas
-            </span>
+                 <p class="has-text-centered is-size-6-mobile">
+                     SOMOS:
+                 </p>
+             </span>
+                        <a class="has-text-info is-size-6-mobile">
+                            <p class="has-text-centered has-text-weight-bold">
+                                Empresa 100% mexicana
+                            </p>
+                        </a>
+                        <a class="has-text-black is-size-7-mobile">
+                            <p class="has-text-justified is-family-secondary p1margen">
+                                Dedicada a la distribución de productos alimenticios 
+                                y especialistas en restaurantes, restaurantes gourmet,
+                                comedores industriales y ventas por mayore.
+                                Ofrecemos un servicio de calidad en cada uno de nuestros
+                                productos ya que sabemos lo importante que es una
+                                alimentación sana para todo ser humano.
+                            </p>
+                        </a>
         </div>
-        <div class="column">
-            <span class="title is-6 is-mega-menu-title">
-                que tu quieras
-            </span>
-        </div>
-        <div class="column">
-            <span class="title is-6 is-mega-menu-title">
-                aqui
-            </span>
+        <div class="column is-full-mobile is-one-thrid-tablet is-one-third-desktop is-one-third-widescreen is-one-third-fullhd">
+                    <figure class="image is-2by1">
+                        <img src="../assets/carrusel/img_dos.jpeg" alt="imagen 5">
+                    </figure>
         </div>
     </div>
 
+    </div>
+
 </template>
+<style scoped>
+.p1margen{
+    padding-left: 1rem;
+    padding-right: 1rem;
+}
+/* .contenedor{
+    position: relative;
+    display: inline-block;
+    text-align: center;
+}
+.p1position{
+    position: absolute;
+    top: 50%; left: 50%;
+    transform: translate(-50%, -50%);
+} */
+</style>
