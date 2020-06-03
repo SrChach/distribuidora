a87478687e76188392013a47f368575790c444a4
Mario Nataren <mario.nataren@gmail.com>
2020-06-03 02:05:48 -0500

    Se completo catalogo de productos y se modifico la vista products.vue para renderizar las imagenes.

--- src/data/products.js
+++ src/data/products.js
@@ -1,79 +1,446 @@
 export default [
     {
         id: 1,
-        name: 'Pollo rostizado',
+        name: 'Ala',
         category: 'Pollo',
-        price: 21.50,
-        description: 'rico pollo con salsa y aderecito especiales'
+        price: 0.0,
+        path: 'stockCatalogo/pollo/alanatural2.jpg',
+        description: ''
     },
     {
         id: 2,
-        name: 'Huevo fresco',
-        category: 'Huevo',
-        price: 32.50,
-        description: 'Huevo de rancho si señor'
+        name: 'Alitas Enchiladas',
+        category: 'Pollo',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/pollo/alitasenchiladas1.jpg'
     },
     {
         id: 3,
-        name: 'Biffe',
-        category: 'Carnes rojas',
-        price: 75.32,
-        description: 'Carne roja con un corte de la mejor calidad, grueso y jugoso, listo para disfrutar con los mejores ingredientes blah blah'
+        name: 'Alitas Naturales',
+        category: 'Pollo',
+        price: 0.0,
+        description: '',
+        path:'stockCatalogo/pollo/alitasnaturales2.jpg'
     },
     {
         id: 4,
-        name: 'Bisteck',
-        category: 'Carnes rojas',
-        price: 58.50,
-        description: 'Para la preparación de las mejores carnes asadas'
+        name: 'Milanesa de Pollo',
+        category: 'Pollo',
+        price: 0.0,
+        path:'stockCatalogo/pollo/milanesa de pollo2.jpg',
+        description: ''
     },
     {
         id: 5,
-        name: 'Pechuga premium',
+        name: 'Carne Molida',
         category: 'Pollo',
-        price: 50.50,
-        description: 'La carne más saludable y con menos grasa'
+        price: 0.0,
+        path:'stockCatalogo/pollo/molida1.jpg',
+        description: ''
     },
     {
         id: 6,
-        name: 'Huevo cascado',
-        category: 'Huevo',
-        price: 11.00,
-        description: 'Huevo más barato por venir maltratado'
+        name: 'Nuggets',
+        category: 'Pollo',
+        price: 0.0,
+        path:'stockCatalogo/pollo/nugget3.jpg',
+        description: ''
     },
     {
         id: 7,
-        name: 'Carne molida',
-        category: 'Carnes rojas',
-        price: 49.00,
-        description: 'Carne molida para la preparación de ricas albohóndigas'
+        name: 'Pechuga',
+        category: 'Pollo',
+        price: 0.0,
+        path: 'stockCatalogo/pollo/pechuga pollo.jpeg',
+        description: ''
     },
     {
         id: 8,
-        name: 'Costilla',
-        category: 'Carnes rojas',
-        price: 50.99,
-        description: 'Costillitas listas para preparar'
+        name: 'Pollo Entero',
+        category: 'Pollo',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/pollo/pollo entero.jpeg'
     },
     {
         id: 9,
-        name: 'Menudencia',
+        name: 'Pulpa',
         category: 'Pollo',
-        price: 21.15,
-        description: 'Piezas de pollo con sabor extraño xD'
+        price: 0.0,
+        path:'stockCatalogo/pollo/pulpa2.jpg',
+        description: ''
     },
     {
         id: 10,
-        name: 'Mojarra',
-        category: 'Pescado',
-        price: 68.50,
-        description: 'La única pieza de pescado que vendemos'
+        name: 'Retazo',
+        category: 'Pollo',
+        price: 0.0,
+        path:'stockCatalogo/pollo/retazo2.jpg',
+        description: ''
     },
     {
         id: 11,
-        name: 'Pata',
-        category: 'Carnes rojas',
-        price: 43.50,
-        description: 'Patitas de res, incluyen la receta de la abuela'
+        name: 'Tender',
+        category: 'Pollo',
+        price: 0.0,
+        path: 'stockCatalogo/pollo/tender1.jpg',
+        description: ''
+    },
+    {
+        id: 12,
+        name: 'Pierna y muslo',
+        category: 'Pollo',
+        price: 0.0,
+        path: 'stockCatalogo/pollo/pierna y muslo .jpeg',
+        description: ''
+    },
+    //carnes
+    {
+        id: 14,
+        name: 'Arrachera',
+        category: 'Res',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/res/arrachera1.jpeg'
+    },
+    {
+        id: 15,
+        name: 'Bistec de res',
+        category: 'Res',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/res/bistec de res1.jpeg'
+    },
+    {
+        id: 16,
+        name: 'Cecina',
+        category: 'Res',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/res/cecina1.jpeg'
+    },
+    {
+        id: 17,
+        name: 'Chambarete sin hueso',
+        category: 'Res',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/res/chamabarete s:hueso3.jpeg'
+    },
+    {
+        id: 18,
+        name: 'Chambarete con hueso',
+        category: 'Res',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/res/chambarete c:hueso3.jpeg'
+    },
+    {
+        id: 19,
+        name: 'Cowboy',
+        category: 'Res',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/res/cowboy1.jpeg'
+    },
+    {
+        id: 20,
+        name: 'Falda',
+        category: 'Res',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/res/falda1.jpeg'
+    },
+    {
+        id: 21,
+        name: 'Molida',
+        category: 'Res',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/res/molida .jpeg'
+    },
+    {
+        id: 22,
+        name: 'New York',
+        category: 'Res',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/res/new york1.jpeg'
+    },
+    {
+        id: 23,
+        name: 'Rib eye',
+        category: 'Res',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/res/rib eye1.jpeg'
+    },
+    {
+        id: 24,
+        name: 'Tbone',
+        category: 'Res',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/res/tbone1.jpeg'
+    },
+    //cerdo
+    {
+        id: 25,
+        name: 'Chicharron Prensado',
+        category: 'Cerdo',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/cerdo/chicharron2.jpeg'
+    },
+    {
+        id: 26,
+        name: 'Chuleta Ahumada',
+        category: 'Cerdo',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/cerdo/chuleta ahumada.jpeg'
+    },
+    {
+        id: 27,
+        name: 'Chuleta',
+        category: 'Cerdo',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/cerdo/chuletanatural3.jpeg'
+    },
+    {
+        id: 28,
+        name: 'Espinazo',
+        category: 'Cerdo',
+        price: 0.0,
+        path: 'stockCatalogo/cerdo/espinazo1.jpeg',
+        description:''
+    },
+    {
+        id: 29,
+        name: 'Longaniza',
+        category: 'Cerdo',
+        price: 0.0,
+        path: 'stockCatalogo/cerdo/longaniza2.jpeg',
+        description:''
+    },
+    {
+        id: 30,
+        name: 'Carne Al Pastor',
+        category: 'Cerdo',
+        price: 0.0,
+        path: 'stockCatalogo/cerdo/pastor2.jpeg',
+        description:''
+    },
+    {
+        id: 31,
+        name: 'Pierna',
+        category: 'Cerdo',
+        price: 0.0,
+        path: 'stockCatalogo/cerdo/pierna1.jpeg',
+        description:''
+    },
+    {
+        id: 32,
+        name: 'Tocino',
+        category: 'Cerdo',
+        price: 0.0,
+        path: 'stockCatalogo/cerdo/tocino3.jpeg',
+        description:''
+    },
+    //frutas
+    {
+        id: 33,
+        name: 'Fresa',
+        category: 'Frutas',
+        price: 0.0,
+        path: 'stockCatalogo/frutas/fresa3.jpg',
+        description:''
+    },
+    {
+        id: 34,
+        name: 'Guayaba',
+        category: 'Frutas',
+        price: 0.0,
+        path: 'stockCatalogo/frutas/guayaba2.jpg',
+        description:''
+    },
+    {
+        id: 35,
+        name: 'Jicama',
+        category: 'Frutas',
+        price: 0.0,
+        path: 'stockCatalogo/frutas/jicama1.jpeg',
+        description:''
+    },
+    {
+        id: 36,
+        name: 'Mango',
+        category: 'Frutas',
+        price: 0.0,
+        path: 'stockCatalogo/frutas/mango3.jpeg',
+        description:''
+    },
+    {
+        id: 37,
+        name: 'Manzana',
+        category: 'Frutas',
+        price: 0.0,
+        path: 'stockCatalogo/frutas/manzana2.jpeg',
+        description:''
+    },
+    {
+        id: 38,
+        name: 'Melon',
+        category: 'Frutas',
+        price: 0.0,
+        path: 'stockCatalogo/frutas/melon1.jpeg',
+        description:''
+    },
+    {
+        id: 39,
+        name: 'Naranja',
+        category: 'Frutas',
+        price: 0.0,
+        path:'stockCatalogo/frutas/naranja2.jpeg',
+        description: ''
+    },
+    {
+        id: 40,
+        name: 'Papaya',
+        category: 'Frutas',
+        price: 0.0,
+        path: 'stockCatalogo/frutas/papaya2.jpeg',
+        description: ''
+    },
+    {
+        id: 41,
+        name: 'Pera',
+        category: 'Frutas',
+        price: 0.0,
+        path: 'stockCatalogo/frutas/pera3.jpeg',
+        description: ''
+    },
+    {
+        id: 42,
+        name: 'Piña',
+        category: 'Frutas',
+        price: 0.0,
+        path:'stockCatalogo/frutas/pinÌa2.jpeg',
+        description: ''
+    },
+    {
+        id: 43,
+        name: 'Sandía',
+        category: 'Frutas',
+        price: 0.0,
+        path: 'stockCatalogo/frutas/sandia2.jpeg',
+        description: ''
+    },
+    {
+        id: 44,
+        name: 'Uva',
+        category: 'Frutas',
+        price: 0.0,
+        path: 'stockCatalogo/frutas/uva3.jpeg',
+        description: ''
+    },
+    //verduras
+    {
+        id: 45,
+        name: 'Aguacate',
+        category: 'Verduras',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/verduras/aguacate3.jpg'
+    },
+    {
+        id: 46,
+        name: 'Calabaza',
+        category: 'Verduras',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/verduras/calabazalarga3.jpg'
+    },
+    {
+        id: 47,
+        name: 'Cebolla',
+        category: 'Verduras',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/verduras/cebolla1.jpg'
+    },
+    {
+        id: 48,
+        name: 'Chayote',
+        category: 'Verduras',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/verduras/chayote1.jpg'
+    },
+    {
+        id: 49,
+        name: 'Jitomate',
+        category: 'Verduras',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/verduras/jitomate3.jpeg'
+    },
+    {
+        id: 50,
+        name: 'Limon',
+        category: 'Verduras',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/verduras/limon3.jpg'
+    },
+    {
+        id: 51,
+        name: 'Nopal',
+        category: 'Verduras',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/verduras/nopal1.jpg'
+    },
+    {
+        id: 52,
+        name: 'Papa',
+        category: 'Verduras',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/verduras/papa1.jpg'
+    },
+    {
+        id: 53,
+        name: 'Romana',
+        category: 'Verduras',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/verduras/romana3.jpg'
+    },
+    {
+        id: 54,
+        name: 'Sangria',
+        category: 'Verduras',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/verduras/sangria2.jpeg'
+    },
+    {
+        id: 55,
+        name: 'Tomate',
+        category: 'Verduras',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/verduras/tomate3.jpg'
+    },
+    {
+        id: 56,
+        name: 'Zanahoria',
+        category: 'Verduras',
+        price: 0.0,
+        description: '',
+        path: 'stockCatalogo/verduras/zanahoria2.jpg'
     }
 ]
