b142ad35fd4f7f3ceca91c32459134365f5e850a
Mario Nataren <mario.nataren@gmail.com>
2020-06-10 00:51:05 -0500

    Propuesta de header, tipo saltarin

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="container is-fluid">
+        <!-- <div class="container is-fluid">
             <div class="columns is-multiline">
                 <div class="column is-12" style="padding-bottom: 0">
                     <figure class="image container has-text-centered">
@@ -15,6 +15,14 @@
         </div>
         <br>
         <b-navbar close-on-click :fixed-top="!is_top" type="is-info" id="nav">
+            <template slot="start" :transparent="true">
+                <b-navbar-item>
+                    <img
+                        src="@/assets/LogoHeader.jpeg"
+                        alt="Logo de Distribuidora Conchita"
+                    >
+                </b-navbar-item>
+            </template>
             <template slot="start" :transparent="true">
                 <b-navbar-item tag="router-link" :to="{ path: '/' }"
                 class="is-size-5-desktop has-text-weight-bold">
@@ -32,11 +40,29 @@
                     <b>Contáctanos</b>&nbsp;&nbsp;
                 </b-navbar-item>
             </template>
-        </b-navbar>
+        </b-navbar> -->
+        <figure class="image container has-text-centered">
+            <img
+                class="is-rounded responsive"
+                src="@/assets/LogoHeader.jpeg"
+                alt="Logo de Distribuidora Conchita"
+            >
+        </figure>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <nav class="menu" id="myTopnav">
+            <a href="#" class="active" title="Home"><i></i> Productos </a>
+            <a href="#" title="About"><i class="far fa-address-card"></i> Nosotro </a>
+            <a href="#" title="Contact"><i class="fas fa-mobile-alt"></i> Contactanos </a>
+            <a href="javascript:void(0);" class="icon" @submit.prevent="myFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
+        </nav>
     </div>
 </template>
 
 <script>
+
 export default {
     data: function () {
         return {
@@ -52,8 +78,17 @@ export default {
     methods: {
         handleScroll () {
             this.is_top = (window.scrollY <= 120) ? true : false;
-        }
+        },
+        myFunction () {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "menu") {
+                x.className += " responsive";
+            } else {
+                x.className = "menu";
+            }
+        } 
     }
+    
 }
 </script>
 
@@ -68,4 +103,110 @@ export default {
     display: block;
     margin: auto;
 }
+@-webkit-keyframes up-underline {
+    0% { padding: 22px 0px; }    
+    100% { padding: 6px 0px; }
+}
+ 
+@-moz-keyframes up-underline {
+    0% { padding: 22px 0px; }    
+    100% { padding: 6px 0px; }
+}
+ 
+@-o-keyframes up-underline {
+    0% { padding: 22px 0px; }    
+    100% { padding: 6px 0px; }
+}
+ 
+@keyframes up-underline {
+    0% { padding: 22px 0px; }    
+    100% { padding: 6px 0px; }
+}
+ 
+@-webkit-keyframes down-underline {
+    0% { padding: 6px 0px; }    
+    100% { padding: 22px 0px; }
+}
+ 
+@-moz-keyframes down-underline {
+    0% { padding: 6px 0px; }    
+    100% { padding: 22px 0px; }
+}
+ 
+@-o-keyframes down-underline {
+    0% { padding: 6px 0px; }    
+    100% { padding: 22px 0px; }
+}
+ 
+@keyframes down-underline {
+    0% { padding: 6px 0px; }    
+    100% { padding: 22px 0px; }
+}
+
+@media screen and (max-width: 600px) {
+  .menu a:not(:first-child) {display: none;}
+  .menu a.icon {
+    float: right;
+    display: block;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .menu .responsive {position: relative;}
+  .menu .responsive a.icon {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+  .menu .responsive a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+}
+body{
+    margin:0;
+}
+.menu .icon {
+  display: none;
+}
+.menu{
+    text-align: center;
+    line-height: 3.5;
+    border-bottom: 4px solid #E91E63;
+}
+ 
+    .menu a{
+        padding: 22px 0px;
+        margin-right: 40px;
+        font-family: arial;
+        font-size: 18px;
+        text-decoration: none;
+        color: #282929;
+        border-bottom: 3px solid #e91e63;
+         
+        -webkit-animation: down-underline .4s;
+        -moz-animation: down-underline .4s;
+        -o-animation: down-underline .4s;
+        animation: down-underline .4s;
+ 
+        -webkit-transition: all .2s ease-in;
+        -moz-transition: all .2s ease-in;
+        -o-transition: all .3s ease-in;
+        transition: all .3s ease-in;
+    }
+ 
+    .menu a:hover{
+        color:#e91e63;
+        padding: 6px 0px;
+             
+        -webkit-animation: up-underline .4s;
+        -moz-animation: up-underline .4s;
+        -o-animation: up-underline .4s;
+        animation: up-underline .4s;
+    }
+ 
+    .menu a.active{
+        padding: 6px 0;
+    }
 </style>
