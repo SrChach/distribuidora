af22cca999cd4d316ee67f98b977935dd00bf2a9
srchach <gsikmo@gmail.com>
2020-07-04 18:58:58 -0500

    REFACTOR: Moved modal to component. To check if GH-Actions is working

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,7 @@
       <slider/>
     </div>
     <div id="main-content">
-
-      <b-modal :active.sync="is_modal_active">
-        <div class="container">
-          <div class="columns is-centered">
-            <div class="card column is-10 bg-white">
-              <p class="title">
-                La página real se encuentra bajo construcción.<br>
-                Los datos aquí mostrados son ficticios, y deberán usarse SOLO como referencia.
-              </p>
-            </div>
-          </div>
-        </div>
-      </b-modal>
-
+      <modal-en-construccion/>
       <router-view/>
     </div>
     <custom-footer/>
@@ -26,23 +13,20 @@
 </template>
 
 <script>
+/**Components */
 import slider from '@/components/Slider.vue'
 import navbar from '@/components/Header.vue'
 import footer from '@/components/Footer.vue'
+
+/**Only gh-pages components */
+import EnConstruccion from '@/components/EnConstruccion.vue'
 
 export default {
   components: {
     'slider': slider,
     'navbar': navbar,
-    'custom-footer': footer
-  },
-  data: function () {
-    return {
-      is_modal_active: false
-    }
-  },
-  mounted: function () {
-    this.is_modal_active = true
+    'custom-footer': footer,
+    'modal-en-construccion': EnConstruccion
   }
 }
 </script>
---
+++ src/components/EnConstruccion.vue
@@ -0,0 +1,36 @@
+<template>
+    <b-modal :active.sync="is_modal_active">
+        <div class="container">
+            <div class="columns is-centered">
+            <div class="column is-10">
+                <div class="card bg-white">
+                <header class="card-header">
+                    <p class="card-header-title has-text-danger">
+                        Advertencia
+                    </p>
+                </header>
+                <div class="card-content">
+                    <div class="content">
+                        La página real se encuentra bajo construcción.<br>
+                        Los datos aquí mostrados <b>son ficticios</b>, y deberán usarse <b>SOLO</b> como referencia.
+                    </div>
+                </div>              
+                </div>
+            </div>
+            </div>
+        </div>
+    </b-modal>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            is_modal_active: false
+        }
+    },
+    mounted: function () {
+        this.is_modal_active = true
+    }
+}
+</script>
